dc14786669e9a6ce42aaf463ebe476aa18037d2b
Peter Ahlgren <ahlgren1234@gmail.com>
2022-02-16 11:59:11 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Peter Ahlgren on 2022-02-16.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
